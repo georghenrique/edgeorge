42dd766a61580c65e9032f197a1a74f992aacc30
João Paulo Preti <jppreti@gmail.com>
2023-03-10 18:01:53 -0400

    inplementei e comentei o codigo de ponteiros simples

--- ponteiro.c
+++ ponteiro.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main() {
+    
     int number = 10;
     int *numberP = &number;
     printf("Valor da variável number: %d\n",number);
@@ -10,17 +11,18 @@ int main() {
     printf("Valor apontado por numberP: %d\n\n",*numberP);
     printf("Mudando o valor de number de forma indireta\n\n");
     *numberP = 15;
-    printf("Valor da variável number: %d\n",number);
+    printf("Valor da variável number Mmodificado: %d\n",number);
+    printf("Incremento da variavel ponteiro\n");
     numberP++;
-    printf("Valor da variável numberP: %p\n", numberP);
+    printf("Valor da variável numberP incrementada: %p\n", numberP);
     printf("Valor no endereço apontado: %d\n", *numberP);
-    float *pi;
-    printf("Valor da variável pi: %p\n", pi);
-    pi = (float *) malloc(sizeof(float));
+    printf("criação de uma variavel *pi dinamicamente\n");
+    float *pi = (float *) malloc(sizeof(float));
     printf("Valor da variável pi: %p\n", pi);
     *pi=3.1415;
     printf("Valor no endereço apontado: %f\n", *pi);
     free(pi);
+    printf("liberando a variavel pi com a função free\n");
     printf("Valor da variável pi: %p\n", pi);
     printf("Valor da variável apontada: %f\n", *pi);
     *pi = 0;
@@ -31,10 +33,12 @@ int main() {
     *(numbers+2) = 3;
 
     for (int i = 0; i < 3; i++)
-        printf("numbers[%d] = %d\n",i,numbers[i]);
+        printf("*(numbers+%d) = %d\n",i,*(numbers+i));
+
 
     for (int i = 0; i < 3; i++)
-        printf("*(numbers+%d) = %d\n",i,*(numbers+i));
+        printf("numbers[%d] = %d\n",i,numbers[i]);
+
 
     int *aux = numbers;
     for (int i = 0; i < 3; i++) {
