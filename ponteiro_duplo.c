0d1f36a1521bd708de9751283d3c8a80d3e64302
João Paulo Preti <jppreti@gmail.com>
2023-03-11 12:40:01 -0400

    implementei a funcao ponteiro_duplo, mas tenho uma duvida

--- ponteiro_duplo.c
+++ ponteiro_duplo.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAMANHO 2
+
+void calculaPi(float *pi) {
+    pi = (float *) malloc(sizeof(float));
+    printf("Valor de pi depois de alocado: %p\n", pi);
+    *pi = 3.1415;
+    printf("Valor de pi: %f\n", *pi);
+}
+
+void calculaPi1(float **pi) {
+  *pi = (float *) malloc(sizeof(float));
+  **pi = 3.1415;
+}
+
 
 int main(){
     int number = 10;
@@ -13,5 +27,46 @@ int main(){
     printf("Valor da variável numberPP: %p\n", numberPP);
     printf("Valor de *numberPP: %p\n", *numberPP);
     printf("Valor **numberPP: %d\n", **numberPP);
+    
+    printf("\n================================================\n");
+    int i;
+    int **matriz;
+    matriz = (int **) malloc(sizeof(int *) * TAMANHO);
+    if (matriz == NULL) {
+        printf("Erro ao alocar memória");
+        free(matriz);
+    }
+
+    for (i = 0; i < TAMANHO; i++) {
+        *(matriz+i) = (int *) malloc(TAMANHO * sizeof(int));
+        if (*(matriz+i) == NULL) {
+            printf("Erro ao alocar memória");
+            free(matriz);
+        }
+    }
+
+    *(*(matriz+0)+0) = 1;
+    *(*(matriz+0)+1) = 2;
+    *(*(matriz+1)+0) = 3;
+    *(*(matriz+1)+1) = 4;
+
+    printf("%d %d\n", matriz[0][0], matriz[0][1]);
+    printf("%d %d\n", matriz[1][0], matriz[1][1]);
+
+    free(matriz);
+
+    printf("\n================================================\n");
+    
+    /*float *pi;
+    calculaPi(&pi);
+    printf("Valor de pi: %p\n", pi);
+    printf("Valor de pi: %f\n", *pi);*/
+
+    printf("\n================================================\n");
+    
+    float *pi = NULL;
+    calculaPi1'z(&pi);
+    printf("Valor de pi: %p\n", pi);
+    printf("Valor do conteúdo apontado por pi: %.4f\n", *pi);
     return EXIT_SUCCESS;
 }
