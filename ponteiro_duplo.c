4b323420dd10dff976b7c20067ced5aee2a14353
João Paulo Preti <jppreti@gmail.com>
2023-03-10 20:29:32 -0400

    testei o funcionamento do ponteiro duplo

--- ponteiro_duplo.c
+++ ponteiro_duplo.c
@@ -1,36 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAMANHO 2
+int main(){
+    int number = 10;
+    int *numberP = &number;
+    int **numberPP = &numberP;
 
-int i;
-int **matriz;
-
-int main() {
-    matriz = (int **) malloc(sizeof(int *) * TAMANHO);
-    if (matriz == NULL) {
-        printf("Erro ao alocar memória");
-
-    free(matriz);
-    }
-
-    for (i = 0; i < TAMANHO; i++) {
-        *(matriz+i) = (int *) malloc(TAMANHO * sizeof(int));
-        if (*(matriz+i) == NULL) {
-            printf("Erro ao alocar memória");
-            free(matriz);
-        }
-    }
-
-    *(*(matriz+0)+0) = 1;
-    *(*(matriz+0)+1) = 2;
-    *(*(matriz+1)+0) = 3;
-    *(*(matriz+1)+1) = 4;
-
-    printf("%d %d\n", matriz[0][0], matriz[0][1]);
-    printf("%d %d\n", matriz[1][0], matriz[1][1]);
-
-    free(matriz);
-
+    printf("Valor da variável number: %d\n", number);
+    printf("Endereço da variável number: %p\n", &number);
+    printf("Valor da variável numberP: %p\n", numberP);
+    printf("Endereço da variável numberP: %p\n", &numberP);
+    printf("Valor da variável numberPP: %p\n", numberPP);
+    printf("Valor de *numberPP: %p\n", *numberPP);
+    printf("Valor **numberPP: %d\n", **numberPP);
     return EXIT_SUCCESS;
 }
