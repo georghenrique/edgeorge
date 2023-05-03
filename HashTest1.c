cc1cc15317085b297e6e148d02a31d8cb82d2992
João Paulo Preti <george.gh_henrique@hotmail.com>
2023-05-03 12:21:04 -0400

    corrigir os bugs

--- HashTest1.c
+++ HashTest1.c
@@ -46,13 +46,32 @@ int main() {
      }
      fclose(file);
 
+    
+    
+    /*
+    log_info("Calculo do maior numero de colisoes:");
+    int cont,i = 0;
+    HashStruct * a = &has;
+    while(i<MAX){
+        log_trace("while <-");
+        if(cont<a->hashes[i].size){
+            log_trace("if <-");
+            log_debug("valor de size na posicao %d é: %d", i, a->hashes[i].size);
+            cont = a->hashes[i].size;
+            log_debug("contador de colisoes recebe size: %d", cont);
+        }
+        log_trace("if ->");
+        i++;
+    }
+    log_trace("while ->");*/
+    
+    /*
+    //inplementação antigas com problemas
     log_info("Calculo do maior numero de colisoes:");
     int cont,i = 0;
     HashStruct * a = &has;
     Node *aux = &a->hashes[i].first->next;
     int teste = a->hashes[i].size;
-            log_debug("teste: %d", teste);
-
     log_info("aux recebeu o endereco de memoria de HashStruct na 1 posicao");
     while((aux != a->hashes[i].first)==true && i<MAX){
         log_trace("while <-");
@@ -68,21 +87,100 @@ int main() {
         log_trace("aux = receber next");
         i++;
     }
-
     log_trace("while ->");
-
-    log_debug("maior colisao encontrada: %d", cont);
-
+    log_debug("maior colisao encontrada: %d", cont);*/
 
 
 
 
+    /*
+    const int dimx = 5, dimy = 5;//Dimensão da matriz
+    int k, j;
+    
+    FILE *grafico = fopen("graficoEspalhamento.ppm", "w");
+    //Descrição do arquivo
+    fprintf(grafico, "P6\n%d %d\n255\n", dimx, dimy);
+    //Varre a matriz em coloca uma cor na posição
+    
+    int count=0;
+    for (j = 0; j < dimy; ++j)
+    {
+        for (k = 0; k < dimx; ++k)
+        {
+            while((aux != has.hashes[i].first)==true && i<MAX){
+            if(has.hashes[count].size<250){
+                //tente trocar o unsigned char por outros tipos
+                //Verifique o que significa unsigned char
+                static unsigned char color[3];
+                color[0] = 0;  // red
+                color[1] = 250-has.hashes[count].size; // green
+                log_debug("oq esta caontcendo: %d", has.hashes[count].size);
+                color[2] = 0;  //blue
+                fwrite(color, 1, 3, grafico);//Escreve no arquivo a cor
+            }else{
+                //tente trocar o unsigned char por outros tipos
+                //Verifique o que significa unsigned char
+                static unsigned char color[3];
+                color[0] = 0;  // red
+                color[1] = 0;  // green
+                color[2] = 0;  // blue
+                fwrite(color, 1, 3, grafico);//Escreve no arquivo a cor
+                count++;
+            }
+        aux = a->hashes[i].first->next;
+        log_trace("aux = receber next");
+        i++;
+        }
+        }
+    }
+    fclose(grafico);//Salva o arquivo*/
+    
+    
+    
+    const int dimx = 5, dimy = 5;//Dimensão da matriz
+    int k, j;
+    
+    FILE *grafico = fopen("graficoEspalhamento.ppm", "w");
+    //Descrição do arquivo
+    fprintf(grafico, "P6\n%d %d\n255\n", dimx, dimy);
+    //Varre a matriz em coloca uma cor na posição
+    
+    int count, f, valorMax =0;
+    valorMax = 255/3;
+    for (j = 0; j < dimy; ++j)
+    {
+        for (k = 0; k < dimx; ++k){
+            while(f<MAX){
+                log_trace("while <-");          
+                log_debug("posição do size: %d valor dos size: %d", f, has.hashes[f].size);
+                static unsigned char color[3];
+                if(has.hashes[f].size==0){
+                    log_debug("SIZE É ZERO: %d ", has.hashes[f].size);
+                    static unsigned char color[3];
+                color[0] = 0;  // red
+                color[1] = 0;  // green
+                color[2] = 0;  // blue
+                fwrite(color, 1, 3, grafico);//Escreve no arquivo a cor
 
+                }
+                else{
+                    color[0] = 0;  // red            
+                    color[1] = 3/has.hashes[f].size;
+                    color[2] = 0;  //blue
 
+                    log_debug("valor do ppm: %d ", color[1]);
+                    fwrite(color, 1, 3, grafico);//Escreve no arquivo a cor
+                    
+                }
+                f++;
+                
 
-
-
-
+            }
+            log_trace("while ->");          
+            
+        }
+    }
+    fclose(grafico);
 
 
    
