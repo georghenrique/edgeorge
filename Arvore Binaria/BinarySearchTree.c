4daa7036f869b89e1e8e6a3dd37cd4b7fc0ad0be
João Paulo Preti <george.gh_henrique@hotmail.com>
2023-05-22 21:51:00 -0400

    criada os arquivos da arvores

---
+++ Arvore Binaria/BinarySearchTree.c
@@ -0,0 +1,22 @@
+#include "BinarySearchTree.h"
+
+int add(TreeNode **root, void *element, TreeComparator f) {
+    if ((*root) == NULL) {
+        // alcançou o local da folha, atualiza o pai
+        TreeNode *newnode = (TreeNode *) malloc(sizeof(TreeNode));
+        if (newnode == NULL)
+            return 0;
+        newnode->element = element;
+        newnode->left = newnode->right = NULL;
+        *root = newnode;
+        return 1;
+    }
+    int compvalue = f(element, (*root)->element);
+    if (compvalue > 0) {
+        return add(&(*root)->right, element, f);
+    } else if (compvalue < 0) {
+        return add(&(*root)->left, element, f);
+    } else {
+        return -1;
+    }
+}
